4fa5597d1703c5a66beec9558fd002c8cbbfe785
Jack <jack@jacks-it-lab.de>
2023-04-18 07:10:50 +0200

    add more sources example

---
+++ 04 - more sources/lib/mylib.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "mylib.h"
+int sum(int a, int b){ 
+  return a+b;
+}
+int diff(int a, int b){
+  return a-b;
+}
---
+++ 04 - more sources/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "lib/mylib.h"
+
+int main(int argc, char *args[]){
+
+    printf("Hello World\n");
+    printf("3+4=%d",sum(3,4));
+}
